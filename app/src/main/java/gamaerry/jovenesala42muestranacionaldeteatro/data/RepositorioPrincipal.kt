6a4056b2f76f25c6f95a5dec0e19d8a2a58f4e69
gamaerry <gamaerry@gmail.com>
2023-06-12 00:17:37 -0600

    agregadas las anotaciones al RepositorioPrincipal

--- app/src/main/java/gamaerry/jovenesala42muestranacionaldeteatro/data/RepositorioPrincipal.kt
+++ app/src/main/java/gamaerry/jovenesala42muestranacionaldeteatro/data/RepositorioPrincipal.kt
@@ -5,18 +5,19 @@ import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 import javax.inject.Singleton
 
+// la función del repositorio es únicamente emitir los
+// resultados de las funciones suspendibles del dao
 @Singleton
 class RepositorioPrincipal
 @Inject
 constructor(private val daoPrincipal: DaoPrincipal) {
+    // es basicamente la ejecucion de una consulta de sql
     fun getListaDeProfesionales(palabrasClave: String) = flow {
         emit(daoPrincipal.operacionGetListaDeProfesionales(palabrasClave))
     }.catch { it.printStackTrace() }
 
+    // puesto que el id es unico, se asegura la emision de un solo profesional
     fun getProfesionalPorId(id: String) = flow {
-        if (id == "")
-            emit(null)
-        else
-            emit(daoPrincipal.operacionGetProfesionalPorId(id))
+        emit(daoPrincipal.operacionGetProfesionalPorId(id))
     }.catch { it.printStackTrace() }
 }
