ecbed16afafead81b9ab6e78b9b3b52342fb3c61
gamaerry <gamaerry@gmail.com>
2023-06-12 00:02:50 -0600

    agregadas las anotaciones al DaoPrincipal

--- app/src/main/java/gamaerry/jovenesala42muestranacionaldeteatro/data/DaoPrincipal.kt
+++ app/src/main/java/gamaerry/jovenesala42muestranacionaldeteatro/data/DaoPrincipal.kt
@@ -9,15 +9,21 @@ import gamaerry.jovenesala42muestranacionaldeteatro.model.ProfesionalDelTeatro
 
 @Dao
 interface DaoPrincipal {
+    // inserta a un profesional a la vez a la base de datos
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun operacionInsertarProfesional(profesional: ProfesionalDelTeatro)
 
-    @Query("SELECT * FROM $NOMBRE_BASE_DE_DATOS WHERE nombre LIKE '%' || :palabrasClave || '%'" +
-            " OR especialidades LIKE '%' || :palabrasClave || '%'" +
-            " OR descripcion LIKE '%' || :palabrasClave || '%'" +
-            " OR estado LIKE '%' || :palabrasClave || '%'")
-    suspend fun operacionGetListaDeProfesionales(palabrasClave: String): List<ProfesionalDelTeatro>
-
+    // busca y regresa al profesional con el id especificado
     @Query("SELECT * FROM profesionales WHERE id = :id")
     suspend fun operacionGetProfesionalPorId(id: String): ProfesionalDelTeatro?
+
+    // busca y regresa una lista de profesionales que cumpla con el criterio de la consulta
+    // (que o la especialidad o la descripcion o el estado coincidan con las palabras clave pasadas)
+    @Query(
+        "SELECT * FROM $NOMBRE_BASE_DE_DATOS WHERE nombre LIKE '%' || :palabrasClave || '%'" +
+                " OR especialidades LIKE '%' || :palabrasClave || '%'" +
+                " OR descripcion LIKE '%' || :palabrasClave || '%'" +
+                " OR estado LIKE '%' || :palabrasClave || '%'"
+    )
+    suspend fun operacionGetListaDeProfesionales(palabrasClave: String): List<ProfesionalDelTeatro>
 }
