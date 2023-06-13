d3a8a76328dea8dbf6a7febd1a358ebf97062331
gamaerry <gamaerry@gmail.com>
2023-06-13 00:19:33 -0600

    agregada la funcion de extension ocultarTeclado()

--- app/src/main/java/gamaerry/jovenesala42muestranacionaldeteatro/extensiones.kt
+++ app/src/main/java/gamaerry/jovenesala42muestranacionaldeteatro/extensiones.kt
@@ -1,5 +1,9 @@
 package gamaerry.jovenesala42muestranacionaldeteatro
 
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
 // funcion de extension para obtener la lista de
 // especialidades de cada entidad ProfesionalDelTeatro,
 // que usaran los botones del DetallesProfesionalesFragment
@@ -7,4 +11,12 @@ fun String.extraerLista(): List<String>{
     return split(",", "y").map { string ->
         string.trim().replaceFirstChar { it.uppercaseChar() }
     }
+}
+
+// ocultarTeclado() se usa a la hora de presionar "buscar" en el fragmento principal
+fun View.ocultarTeclado() {
+    val manejadorDelInputMethod =
+        context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    manejadorDelInputMethod.hideSoftInputFromWindow(windowToken, 0)
+    this.clearFocus()
 }
