be6b94757a4a92cd7964a2c143537ce3599514a4
gamaerry <gamaerry@gmail.com>
2023-06-17 01:41:27 -0600

    creadas las extensiones para el guardado

--- app/src/main/java/gamaerry/jovenesala42muestranacionaldeteatro/extensiones.kt
+++ app/src/main/java/gamaerry/jovenesala42muestranacionaldeteatro/extensiones.kt
@@ -1,13 +1,35 @@
 package gamaerry.jovenesala42muestranacionaldeteatro
 
+import android.app.Activity
 import android.content.Context
+import android.content.SharedPreferences
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+
+// se crea una "variable de extension" para tener
+// siempre una referencia al SharedPreferences principal
+val Activity.myPrefs: SharedPreferences
+    get() = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+
+val Activity.guardados: MutableSet<String>?
+    get() = myPrefs.getStringSet("guardados", emptySet())
+
+
+fun Activity.getGuardados(): MutableSet<String> {
+    return guardados!!
+}
+
+fun Activity.setGuardado(id: Int) {
+    myPrefs.edit().putStringSet(
+        "guardados",
+        guardados?.apply { add(id.toString()) }
+    ).apply()
+}
 
 // funcion de extension para obtener la lista de
 // especialidades de cada entidad ProfesionalDelTeatro,
 // que usaran los botones del DetallesProfesionalesFragment
-fun String.extraerLista(): List<String>{
+fun String.extraerLista(): List<String> {
     return split(",", "y").map { string ->
         string.trim().replaceFirstChar { it.uppercaseChar() }
     }
