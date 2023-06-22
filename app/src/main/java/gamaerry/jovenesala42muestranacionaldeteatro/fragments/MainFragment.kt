1d222fe5e2db0cf5f25b9acfc6d994912b30e046
gamaerry <gamaerry@gmail.com>
2023-06-21 21:51:24 -0600

    implementada la transicion de fragmentos lista

--- app/src/main/java/gamaerry/jovenesala42muestranacionaldeteatro/fragments/MainFragment.kt
+++ app/src/main/java/gamaerry/jovenesala42muestranacionaldeteatro/fragments/MainFragment.kt
@@ -6,55 +6,50 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import gamaerry.jovenesala42muestranacionaldeteatro.R
+import gamaerry.jovenesala42muestranacionaldeteatro.databinding.FragmentListaProfesionalesBinding
+import gamaerry.jovenesala42muestranacionaldeteatro.databinding.FragmentMainBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    // los binding enlazan a las vistas con el codigo
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.navegacion.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.inicio -> requireActivity().supportFragmentManager.popBackStack()
+                R.id.guardados -> requireActivity().supportFragmentManager.beginTransaction()
+                    // usa estas animaciones ya definidas
+                    .setCustomAnimations(
+                        R.anim.entrar_desde_derecha,
+                        R.anim.salir_hacia_izquierda,
+                        R.anim.entrar_desde_izquierda,
+                        R.anim.salir_hacia_derecha
+                    )
+                    // reemplaza (no agrega) el DetallesProfesionalesFragment
+                    .replace(R.id.contenedorDeListas, ListaGuardadosFragment())
+                    // se guarda con la etiqueta correspondiente
+                    .addToBackStack("listaGuardados")
+                    .commit()
+            }
+            true
         }
     }
 
+    // se sobreescriben estos metodos unicamente para
+    // el correcto funcionamiento del nuestro binding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
