4c9729014e3c059c0b7182dc25daacd1a4f309db
gamaerry <gamaerry@gmail.com>
2023-06-21 21:55:49 -0600

    implementado el llenado de los fragmentContainerView

--- app/src/main/java/gamaerry/jovenesala42muestranacionaldeteatro/MainActivity.kt
+++ app/src/main/java/gamaerry/jovenesala42muestranacionaldeteatro/MainActivity.kt
@@ -2,12 +2,10 @@ package gamaerry.jovenesala42muestranacionaldeteatro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.addCallback
-import androidx.core.view.isGone
 import dagger.hilt.android.AndroidEntryPoint
 import gamaerry.jovenesala42muestranacionaldeteatro.databinding.ActivityMainBinding
 import gamaerry.jovenesala42muestranacionaldeteatro.fragments.ListaProfesionalesFragment
+import gamaerry.jovenesala42muestranacionaldeteatro.fragments.MainFragment
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -17,9 +15,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // con esta condicion se asegura que este fragment se lanze una sola vez
-        if (savedInstanceState == null)
+        // con esta condicion se asegura que ambos
+        // FragmentContainerView se llenen una sola vez
+        if (savedInstanceState == null){
             supportFragmentManager.beginTransaction()
-                .add(R.id.contenedorPrincipal, ListaProfesionalesFragment()).commit()
+                .add(R.id.contenedorPrincipal, MainFragment()).commit()
+            supportFragmentManager.beginTransaction()
+                .add(R.id.contenedorDeListas, ListaProfesionalesFragment()).commit()
+        }
     }
 }
