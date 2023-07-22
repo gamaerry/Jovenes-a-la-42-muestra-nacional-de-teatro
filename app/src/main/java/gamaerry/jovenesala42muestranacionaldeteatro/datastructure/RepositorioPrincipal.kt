78896d04911a5c17d8fd44a2fc44cd16171df216
gamaerry <gamaerry@gmail.com>
2023-07-22 12:14:54 -0600

    terminadas las anotaciones del RepositorioPrincipal

--- app/src/main/java/gamaerry/jovenesala42muestranacionaldeteatro/datastructure/RepositorioPrincipal.kt
+++ app/src/main/java/gamaerry/jovenesala42muestranacionaldeteatro/datastructure/RepositorioPrincipal.kt
@@ -19,19 +19,27 @@ constructor(private val daoPrincipal: DaoPrincipal) {
         emit(daoPrincipal.operacionGetListaDeProfesionales(palabrasClave))
     }.catch { it.printStackTrace() }
 
+    // es basicamente la ejecucion de una consulta de sql
+    fun getNombrePorId(id: Int) = flow {
+        emit(daoPrincipal.operacionGetNombrePorId(id))
+    }.catch { it.printStackTrace() }
+
+    // filtra por partes para obtener una consulta adecuada
+    // de acuerdo a los estados especialidades y muestras activas
     fun getProfesionalesPorFiltros(
         estados: List<String>,
         especialidades: List<String>,
         muestra: List<String>
     ) = flow {
+        // obtiene en primer lugar una lisa de profesionales de acuerdo a la lista de estados
         val filtroEstados = daoPrincipal.operacionGetProfesionalesPorEstados(estados)
+        // luego de la lista anterior, filtra aquellos cuya alguna de su
+        // especialida coincida con alguna de la lista de especialidades
         val filtroEspecialidades = filtroEstados.filter { profesional ->
             profesional.especialidades.extraerLista().any { it in especialidades }
         }
+        // finalmente se emitira una lista vacia si esta desmarcada la unica muestra nacional
+        // de teatro hasta ahora (puesto que por el momento todos los profesionales son de esta)
         emit(if (muestra.isEmpty()) emptyList() else filtroEspecialidades)
-    }.catch { it.printStackTrace() }
-
-    fun getNombrePorId(id: Int) = flow {
-        emit(daoPrincipal.operacionGetNombrePorId(id))
     }.catch { it.printStackTrace() }
 }
